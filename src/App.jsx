c7e8b7623be3b204f1e22097023004eea651f944
Caioalves14 <caio.alvesrex@gmail.com>
2024-05-01 11:10:45 -0300

    Adicioando mais componentes como as opções para a resposta do quiz

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <div className='App'>
-        <h1>Quiz de programação</h1>
+        <h1>Quiz de Programação</h1>
         {quizState.gameStage === "Start" && <Welcome />}
         {quizState.gameStage === "Playing" && <Question />}
         {quizState.gameStage === "End" && <GameOver />}
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -2,20 +2,38 @@ import React from 'react'
 import { useContext } from 'react'
 import { QuizContext } from '../context/quiz'
 import "./Question.css";
+import Option from './Option';
 
 const Question = () => {
     const [quizState, dispatch] = useContext(QuizContext);
     
-    const currentQuestion = quizState.questions[quizState.currentQuestion]
+    const currentQuestion = quizState.questions[quizState.currentQuestion];
+
+    const onSelectOption = (option) => {
+        dispatch({
+            type: "CHECK_ANSWER",
+            payload: {answer: currentQuestion.answer, option}
+        })
+    }
 
   return (
     <div id='question'>
         <p>Pergunta {quizState.currentQuestion + 1} de {quizState.questions.length}</p>
         <h2>{currentQuestion.question}</h2>
         <div id='options-container'>
-            <p>Opções</p>
+            {currentQuestion.options.map((option) => (
+                <Option
+                option={option} 
+                key={option} 
+                answer={currentQuestion.answer} 
+                selectOption={() => onSelectOption(option)} />
+            ))}
         </div>
-        <button onClick={() => dispatch({type: "CHANGE_QUESTION"})}>Continuar</button>
+        {quizState.answerSelected && (
+        <button onClick={() => dispatch({type: "CHANGE_QUESTION"})}>
+            Continuar
+        </button>
+        )}
     </div>
   )
 }
